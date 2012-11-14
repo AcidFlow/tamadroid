63541116fd824f518a3bbe1e824b2b8792493469
Furzel <rdestign+github@gmail.com>
2012-11-14 15:06:45 +0100

    Service + incoming message handler

---
+++ src/info/acidflow/tamadroid/tamadroidService/IncomingMessageHandler.java
@@ -0,0 +1,56 @@
+package info.acidflow.tamadroid.tamadroidService;
+
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Check msg.getData() != null ? 
+ * 
+ *
+ */
+
+/**
+ * 
+ * Sort the different message types receivable by the service and call the 
+ * appropriate method on the service with the datas found in the message if available
+ *
+ */
+public class IncomingMessageHandler extends Handler {
+
+	private TamadroidService _service; 
+	
+	public IncomingMessageHandler(TamadroidService service) {
+		super();
+		_service = service;
+	}
+	
+	@Override
+	public void handleMessage(Message msg) {
+		switch(msg.what) {
+		case TamadroidService.MSG_REGISTER_CLIENT: {
+			_service.registerClient(msg.replyTo);
+		} break;
+		case TamadroidService.MSG_UNREGISTER_CLIENT: {
+			_service.unregisterClient();
+		} break;
+		case TamadroidService.MSG_FEED_PET: {
+			_service.feedPet(msg.getData().getDouble(TamadroidService.FOOD_AMOUNT));
+		}
+		break;
+		case TamadroidService.MSG_HEAL_PET: {
+			_service.healPet(msg.getData().getDouble(TamadroidService.HEAL_AMOUNT));
+		}
+		break;
+		case TamadroidService.MSG_PLAY_WITH_PET: {
+			_service.playWithPet(msg.getData().getDouble(TamadroidService.PLAY_AMOUNT));
+		}
+		break;
+		case TamadroidService.MSG_SWITCH_LIGHT: {
+			_service.switchLight(msg.getData().getBoolean(TamadroidService.SWITCH_STATE));
+		}
+		break;
+		default: super.handleMessage(msg);
+		}
+	}
+	
+}
---
+++ src/info/acidflow/tamadroid/tamadroidService/TamadroidService.java
@@ -0,0 +1,79 @@
+package info.acidflow.tamadroid.tamadroidService;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Messenger;
+import android.util.Log;
+import android.widget.Toast;
+
+public class TamadroidService extends Service {
+
+	private static final String LOG_TAG = "TamadroidService";
+	
+	/**
+	 * Message type constants for dialog between the service and the activity
+	 */
+	public static final int MSG_REGISTER_CLIENT = 0;
+	public static final int MSG_UNREGISTER_CLIENT = 1;
+	public static final int MSG_FEED_PET = 2;
+	public static final int MSG_HEAL_PET = 3;
+	public static final int MSG_PLAY_WITH_PET = 4;
+	public static final int MSG_SWITCH_LIGHT = 5;
+
+	/**
+	 * Argument type constant for data transmission through messages 
+	 */
+	public static final String FOOD_AMOUNT = "foodAmount";
+	public static final String HEAL_AMOUNT = "healAmount";
+	public static final String PLAY_AMOUNT = "playAmount";
+	public static final String SWITCH_STATE = "switchState";
+	
+	
+	private Messenger _client; 
+	private final Messenger _messenger = new Messenger(new IncomingMessageHandler(this)); 
+
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		return _messenger.getBinder();
+	}
+
+	@Override
+	public void onCreate() {
+		Toast.makeText(getApplicationContext(), "Creating service", Toast.LENGTH_LONG);
+		super.onCreate();
+	}
+
+	@Override
+	public void onDestroy() {
+		Toast.makeText(getApplicationContext(), "Destroying service", Toast.LENGTH_LONG);
+		super.onDestroy();
+	}
+
+	public void registerClient(Messenger replyTo) {
+		_client = replyTo;
+		Log.i(LOG_TAG, "Registering client");
+	}
+	
+	public void unregisterClient() {
+		_client = null;
+		Log.i(LOG_TAG, "Unregistering client");
+	}
+	
+	public void feedPet(double amount) {
+		Log.i(LOG_TAG, "Feeding pet : " + amount);
+	}
+	
+	public void healPet(double amount) {
+		Log.i(LOG_TAG, "Healing pet : " + amount);
+	}
+	
+	public void playWithPet(double amount) {
+		Log.i(LOG_TAG, "Playing with pet : " + amount);
+	}
+	
+	public void switchLight(boolean state) {
+		Log.i(LOG_TAG, "Switching light, light is now : " + state);
+	}
+}
